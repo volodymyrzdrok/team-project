a908a1cec537c77f42e3096366a0dd464600a562
Volodymyr Zdrok <volodymyrzdrok44@gmail.com>
2022-11-09 03:03:08 +0200

    have finished header

---
+++ src/js/mobile-menu.js
@@ -0,0 +1,17 @@
+(() => {
+  const menuBtnRef = document.querySelector('[data-menu-button]');
+
+  const mobileNavRef = document.querySelector('[data-menu-nav]');
+
+  menuBtnRef.addEventListener('click', () => {
+    const expanded =
+      menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
+
+    mobileNavRef.classList.toggle('nav-menu__cont');
+    menuBtnRef.classList.toggle('is-open-menu');
+    menuBtnRef.setAttribute('aria-expanded', !expanded);
+
+    document.body.classList.toggle('modal-open');
+    //  mobileNavRef.classList.toggle('is-open-menu-backdrop');
+  });
+})();
